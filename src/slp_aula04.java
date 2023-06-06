7d441d0ca2dce533c192df08018abdda1980268b
Claytomg <claytomguilherme@gmail.com>
2023-06-06 16:21:11 -0400

    Adicionado arquivo de aula sobre arrays em java nomenclatura slp_aula04

---
+++ src/slp_aula04.java
@@ -0,0 +1,10 @@
+public class slp_aula04 {
+    public static void main(String[] args){
+        String[] vet = new String[]{"Leonardo","joão","Maria","Claytom"};
+        System.out.println("Os nomes informados foram: ");
+        for (int cont = 0; cont < vet.length; cont++)
+        {
+            System.out.println(vet[cont]);
+        }
+    }
+}
