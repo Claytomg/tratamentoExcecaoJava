502d8375cd08e96e2568161a95926e17c7a9ae98
Claytomg <claytomguilherme@gmail.com>
2023-06-06 16:37:45 -0400

    Adicionado leitura de arquivo txt via linguagem Java

---
+++ src/LeituraArquivoTxt.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeituraArquivoTxt {
+    public static void main(String[] args){
+        try{
+            File arquivo = new File("C:/Users/clayt/OneDrive/Imagens/psd/Poema.txt");
+            Scanner scanner = new Scanner(arquivo);
+            while (scanner.hasNextLine()){
+                String linha = scanner.nextLine();
+                System.out.format("%s%n", linha);
+            }
+        } catch (FileNotFoundException ex){
+            System.out.format("Erro abertura do arquivo: %s.%n",ex.getMessage());
+        }
+    }
+
+}
